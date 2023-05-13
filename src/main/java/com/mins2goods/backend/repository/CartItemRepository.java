059db4c766a27be02511f2e964934453ed299b5f
Tomal <tomal11902012@gmail.com>
2023-05-13 15:50:53 +0600

    Cart Delete

--- src/main/java/com/mins2goods/backend/repository/CartItemRepository.java
+++ src/main/java/com/mins2goods/backend/repository/CartItemRepository.java
@@ -2,7 +2,9 @@ package com.mins2goods.backend.repository;
 
 import com.mins2goods.backend.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,7 +13,9 @@ import java.util.List;
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     @Query("SELECT ci FROM CartItem ci WHERE ci.buyer.userId = :buyerId")
     List<CartItem> findByBuyerId(Long buyerId);
-    @Query("DELETE  FROM CartItem ci WHERE ci.buyer.userId = :buyerId")
-    void deleteAllByBuyerId(Long buyerId);
+    @Modifying
+    @Query("DELETE FROM CartItem ci WHERE ci.buyer.userId = :buyerId")
+    void deleteAllByBuyerId(@Param("buyerId") Long buyerId);
+
 }
 
